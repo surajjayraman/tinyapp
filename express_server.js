d09e1f10c79a74d483085385e374aa560c11ea8b
surajjayraman <suraj.jayraman@gmail.com>
2021-09-02 20:06:59 -0400

    moved all helper fns to helpers.js

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const cookieSession = require("cookie-session");
 const bcrypt = require('bcrypt');
 
 //helper module functions
-const {getUserByEmail} = require("./helpers");
+const {getUserByEmail, authenticateUser, urlsForUser, generateRandomString} = require("./helpers");
 
 const app = express();
 const PORT = 8080; // default port 8080
@@ -36,45 +36,6 @@ const users = {
     email: "user2@example.com",
     password: "dishwasher-funk"
   }
-};
-
-// validation: check for user credentials
-const authenticateUser = (email, password, database) => {
-  // loop through the users db => object
-  const user = getUserByEmail(email, database);
-  // check that values of email and password if they match
-  if (user && bcrypt.compareSync(password, user.password)) {
-    // return user id if it matches
-    return user.id;
-  }
-  
-  // default return false
-  return false;
-  
-};
-
-// Helper function which returns the URLs
-// where the userID is equal to the id of
-// the currently logged-in user.
-const urlsForUser = (id, urlDB) => {
-  const userUrls = {};
-  for (const shortURL in urlDB) {
-    if (urlDB[shortURL]['userID'] === id) {
-      userUrls[shortURL] = urlDB[shortURL];
-    }
-  }
-  return userUrls;
-};
-
-/* Generates a random string, used for creating short URLs and userIDs */
-const generateRandomString = function() {
-  let randomString = "";
-  for (let i = 0; i < 6; i++) {
-    const randomCharCode = Math.floor(Math.random() * 26 + 97);
-    const randomChar = String.fromCharCode(randomCharCode);
-    randomString += randomChar;
-  }
-  return randomString;
 };
 
 app.use(bodyParser.urlencoded({extended: true}));
--- helpers.js
+++ helpers.js
@@ -1,4 +1,4 @@
-
+const bcrypt = require('bcrypt');
 // validation: check that the user is not already in the database
 const getUserByEmail = function(email, database) {
   // iterate through the users object
@@ -12,6 +12,48 @@ const getUserByEmail = function(email, database) {
   }
 };
 
+// validation: check for user credentials
+const authenticateUser = (email, password, database) => {
+  // loop through the users db => object
+  const user = getUserByEmail(email, database);
+  // check that values of email and password if they match
+  if (user && bcrypt.compareSync(password, user.password)) {
+    // return user id if it matches
+    return user.id;
+  }
+    
+  // default return false
+  return false;
+    
+};
+
+// Helper function which returns the URLs
+// where the userID is equal to the id of
+// the currently logged-in user.
+const urlsForUser = (id, urlDB) => {
+  const userUrls = {};
+  for (const shortURL in urlDB) {
+    if (urlDB[shortURL]['userID'] === id) {
+      userUrls[shortURL] = urlDB[shortURL];
+    }
+  }
+  return userUrls;
+};
+
+/* Generates a random string, used for creating short URLs and userIDs */
+const generateRandomString = function() {
+  let randomString = "";
+  for (let i = 0; i < 6; i++) {
+    const randomCharCode = Math.floor(Math.random() * 26 + 97);
+    const randomChar = String.fromCharCode(randomCharCode);
+    randomString += randomChar;
+  }
+  return randomString;
+};
+
 module.exports = {
-  getUserByEmail
+  getUserByEmail,
+  authenticateUser,
+  urlsForUser,
+  generateRandomString
 };
